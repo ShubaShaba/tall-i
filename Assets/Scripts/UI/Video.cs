cb7cdcb2f9c586a7441a4314a5a26ff8eaa0cac1
carbonara21 <104208062+carbonara21@users.noreply.github.com>
2024-11-06 00:11:40 +0100

    cutscene

---
+++ Assets/Scripts/UI/Video.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class Video : MonoBehaviour
+{
+
+     [SerializeField] VideoPlayer video;
+
+    void Start()
+    {
+        video.loopPointReached += doSomethingWhenVideoFinish;
+    }
+
+ 
+
+
+     void doSomethingWhenVideoFinish(VideoPlayer vp)
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    void Skip(){
+        if (Input.GetKeyDown(KeyCode.Alpha1)){
+        SceneManager.LoadScene("SampleScene"); 
+        }
+    }
+}
